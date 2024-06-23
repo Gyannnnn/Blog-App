07ea1e3e72e82a024abd7cf47e39d2dc25ddbbfd
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-06-23 09:57:06 +0530

    Added Mongo Link

--- api/index.js
+++ api/index.js
@@ -1,6 +1,22 @@
-import express from 'express'
-const app = express()
+import express from 'express';
+const app = express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config('')
 
-app.listen(3000,()=>{
-    console.log("Server Is Running At 3000")
+mongoose.connect("mongodb+srv://higyanaranjanpatra:n6MVgElSE4ppFNID@blogapp.h2lirt3.mongodb.net/Blog App?retryWrites=true&w=majority&appName=Blogapp")
+.then(() => {
+    console.log("Connected to the database...");
+    console.log("Database is running fine...");
 })
+.catch((err) => {
+    console.log("Error in database connection");
+    console.log(err);
+});
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
