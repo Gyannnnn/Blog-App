13d13e55f5689b0ef017c55317e3bcce081d4ba3
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-06-24 15:09:22 +0530

    Created Signin API

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,22 +1,52 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
+// Signup Function
 export const signup = async (req, res, next) => {
-  const { username, email, password } = req.body; 
-  if (!username || !email || !password || username === "" || email === "" || password === "") {
-    return next(errorHandler(400, 'All fields are required'));
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    return next(errorHandler(400, "All fields are required"));
   }
-  const hashPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({
-    username,
-    email,
-    password: hashPassword, 
-  });
+
   try {
+    const hashPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashPassword });
     await newUser.save();
-    res.status(201).json({ message: "User signed up successfully!", user: req.body });
+    res.status(201).json({ message: "User signed up successfully!", user: { username, email } });
   } catch (err) {
     next(err);
+  }
+};
+
+// Signin Function
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User Not Found!"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(404, "Password is incorrect"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, 'your_jwt_secret_key');
+    const{password:pass,...rest} = validUser._doc
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json({ message: "Signin successful", user: rest });
+  } catch (error) {
+    next(error);
   }
 };
