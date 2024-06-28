358187ef81d237776ad853a9adcf6bcf1d937d02
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-06-28 14:48:03 +0530

    Updated Nav Bar

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,11 +1,13 @@
-import { Navbar, TextInput, Button } from "flowbite-react";
+import { Navbar, TextInput, Button, Dropdown, Avatar } from "flowbite-react";
 import React from "react";
-import { Link ,useLocation} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { IoSearch } from "react-icons/io5";
 import { BsMoonStars } from "react-icons/bs";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-    const path = useLocation().pathname;
+  const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <Navbar className="border-b-2 ">
       <Link
@@ -28,22 +30,55 @@ const Header = () => {
       <Button className="w-10 self-center h-10 lg:hidden" color="gray" pill>
         <IoSearch />
       </Button>
-      <div className="flex gap-2 sm:order-2" >
+      <div className="flex gap-2 sm:order-2">
         <Button className="w-10 h-10" pill color="gray">
           <BsMoonStars />
         </Button>
-        <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
-        <Navbar.Toggle/>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                alt="User avtar"
+                img={currentUser.profilePicture}
+                rounded
+              />
+            }
+          >
+          <Dropdown.Header>
+            <div className="flex flex-col">
+            <span className="text-sm">{currentUser.username}</span>
+            <span className="font-bold">@{currentUser.email}</span>
+            </div>
+          </Dropdown.Header>
+          <Link to="/dashboard?tab=profile">
+            <Dropdown.Item>Profile</Dropdown.Item>
+          </Link>
+            <Dropdown.Divider/>
+            <Link>
+            <Dropdown.Item>Sign Out</Dropdown.Item>
+            </Link>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
+
+        <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link active={path === '/'} as='div'>
+        <Navbar.Link active={path === "/"} as="div">
           <Link to="/">Home</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/about'} as='div'>
-            <Link to="/about">About</Link>
+        <Navbar.Link active={path === "/about"} as="div">
+          <Link to="/about">About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/projects'} as='div'>
-            <Link to='/projects' >Projects</Link>
+        <Navbar.Link active={path === "/projects"} as="div">
+          <Link to="/projects">Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
